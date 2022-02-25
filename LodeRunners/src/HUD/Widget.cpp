e4007e5335bea4ce25084999146afb893c1a30a3
Hypooxanthine <beaujonalexandre@gmail.com>
2022-02-25 22:18:27 +0100

    More in depth implementation of Widget base class : CursorRay class to handle data consuming.

--- LodeRunners/src/HUD/Widget.cpp
+++ LodeRunners/src/HUD/Widget.cpp
@@ -4,9 +4,27 @@ Widget::Widget()
 {
 }
 
+void Widget::update(const float& dt)
+{
+	CursorRay ray;
+	this->update(dt, ray);
+}
+
 const sf::Vector2f& Widget::getGlobalPosition() const
 {
 	if (m_Parent == nullptr) return m_RelativePosition;
 	
 	return m_RelativePosition + m_Parent->getGlobalPosition();
+}
+
+void Widget::updateChildren(const float& dt, CursorRay& ray)
+{
+	for (auto& child : m_Children)
+		child->update(dt, ray);
+}
+
+void Widget::renderChildren(Ref<sf::RenderWindow> window)
+{
+	for (auto& child : m_Children)
+		child->render(window);
 }
--- LodeRunners/src/HUD/Widget.h
+++ LodeRunners/src/HUD/Widget.h
@@ -2,11 +2,30 @@
 
 #include "../Core/Base.h"
 
+class CursorRay
+{
+private:
+	CursorRay() = default;
+
+	inline void consume() { consumed = true; }
+
+	// if(CursorRay) -> condition is true if the ray has not been consumed yet.
+	operator bool() const { return !consumed; }
+
+	const sf::Vector2i& getPosition() const { return m_Position; }
+	const bool& isPressed() const { return m_Pressed; }
+private:
+	// When a CursorRay is used (example : it hovers a button, so the button changes his color and consumes the ray), the widget that uses it has to make this value as true. No verification are done when trying to access data. A widget can see data even if his consumed member is true. He just has to know that another widget has responded to the position member value.
+	bool consumed = false;
+	sf::Vector2i m_Position = sf::Mouse::getPosition(*Application::get()->getWindow());
+	bool m_Pressed = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+};
+
 class Widget
 {
 public:
 	Widget();
-	virtual void update(const float& dt) {}
+	void update(const float& dt); // This function first calculates a CursorRay then internally calls an update function which takes this CursorRay as parameter.
 	virtual void render(Ref<sf::RenderWindow> window) {}
 
 	inline void setParent(Ref<Widget> parent) { m_Parent = parent; }
@@ -16,11 +35,13 @@ public:
 	inline const sf::Vector2f& getRelativePosition() const { return m_RelativePosition; }
 
 protected:
-	inline void renderChildren(Ref<sf::RenderWindow> window) // For more readable code
-	{
-		for (auto& child : m_Children)
-			child->render(window);
-	}
+	virtual void update(const float& dt, CursorRay& ray) {}
+
+	// We must first update children, then update this. If children have something to do with a ray, they handle it and then parent don't. If they don't handle the ray, their parent can handle it, recursively.
+	// Example : a Button widget has a Text widget as child. The Text widget first receives the CursorRay. It obviously has nothing to do with a ray, so it does not use it, and its "consumed" member stays false. Then the Button widget handles the ray. If the ray is inside its bounding box, it consumes it, else, it does not consume it (eventually the ray can be used by the Button widget's parent).
+	void updateChildren(const float& dt, CursorRay& ray); // For more readable code
+	// We must first render this, then children. Children have to show in front of their parent.
+	void renderChildren(Ref<sf::RenderWindow> window); // For more readable code
 
 private:
 	Ref<Widget> m_Parent = nullptr;
@@ -28,4 +49,3 @@ private:
 
 	sf::Vector2f m_RelativePosition;
 };
-
