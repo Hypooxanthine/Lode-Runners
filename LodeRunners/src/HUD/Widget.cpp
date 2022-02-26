ea5b073211d7cbf7036ac3dab9fd7c5f2ece5203
Hypooxanthine <beaujonalexandre@gmail.com>
2022-02-26 01:50:03 +0100

    Widget base class very deep implementation. I hope it is abstracted and modular enough to make it really easy after. That was the initial goal.

--- LodeRunners/src/HUD/Widget.cpp
+++ LodeRunners/src/HUD/Widget.cpp
@@ -2,29 +2,108 @@
 
 Widget::Widget()
 {
+	onResize();
 }
 
 void Widget::update(const float& dt)
 {
-	CursorRay ray;
+	static CursorRay ray; // Instanciated once.
+	ray.create(*m_View); // Called each time.
+
 	this->update(dt, ray);
 }
 
-const sf::Vector2f& Widget::getGlobalPosition() const
+// We must first update children, then update this. If children have something to do with a ray, they handle it and then parent don't. If they don't handle the ray, their parent can handle it. Recursive.
+// Example : a Button widget has a Text widget as child. The Text widget first receives the CursorRay. It obviously has nothing to do with the ray, so it does not use it, and doesn't consume it. Then the Button widget handles the ray. If the ray is inside its bounding box, it consumes it, else, it does not consume it (eventually the ray can be used by the Button widget's parent).
+void Widget::update(const float& dt, CursorRay& ray)
+{
+	updateChildren(dt, ray);
+	updateWidget(dt, ray);
+}
+
+// We must first render "this", then children. Children have to show in front of their parent.
+void Widget::render(Ref<sf::RenderWindow> window)
+{
+	renderWidget(window);
+	renderChildren(window);
+}
+
+void Widget::onResize()
+{
+	const sf::IntRect viewport = getViewport();
+	const sf::Vector2f size = { (float)viewport.width, (float)viewport.height };
+
+	// We want the view's ratio to fit perfectly the viewport ratio.
+	m_View->setCenter(size / 2.f);
+	m_View->setSize(size);
+
+	onResize(size);
+}
+
+// onResize order doesn't particularly counts.
+void Widget::onResize(const sf::Vector2f& size)
+{
+	onWidgetResize(size);
+	onResizeChildren(size);
+}
+
+inline void Widget::setParent(Ref<Widget> parent)
+{
+	m_Parent = parent;
+	m_View = parent->m_View;
+}
+
+inline void Widget::addChild(Ref<Widget> child)
+{
+	m_Children.push_back(child);
+	child->m_View = m_View;
+}
+
+inline sf::Vector2f Widget::getGlobalPosition() const
 {
 	if (m_Parent == nullptr) return m_RelativePosition;
 	
-	return m_RelativePosition + m_Parent->getGlobalPosition();
+	return m_Parent->getGlobalPosition() + m_RelativePosition;
+}
+
+inline void Widget::setRelativePosition(const sf::Vector2f& pos)
+{
+	m_RelativePosition = pos;
+}
+
+inline void Widget::setGlobalPosition(const sf::Vector2f& pos)
+{
+	if (m_Parent)
+		setRelativePosition(pos - m_Parent->getGlobalPosition());
+	else
+		setRelativePosition(pos);
 }
 
 void Widget::updateChildren(const float& dt, CursorRay& ray)
 {
 	for (auto& child : m_Children)
-		child->update(dt, ray);
+		child->updateWidget(dt, ray);
 }
 
 void Widget::renderChildren(Ref<sf::RenderWindow> window)
 {
 	for (auto& child : m_Children)
 		child->render(window);
+}
+
+void Widget::onResizeChildren(const sf::Vector2f& size)
+{
+	for (auto& child : m_Children)
+		child->onResize(size);
+}
+
+inline sf::IntRect Widget::getViewport() const
+{
+	return Application::get()->getWindow()->getViewport(*m_View);
+}
+
+void Widget::setViewport(const sf::FloatRect& viewport)
+{
+	m_View->setViewport(viewport);
+	onResize(); // Triggering onResize for this Widget and every children Widgets
 }
--- LodeRunners/src/HUD/Widget.h
+++ LodeRunners/src/HUD/Widget.h
@@ -4,48 +4,80 @@
 
 class CursorRay
 {
-private:
+public:
 	CursorRay() = default;
+
+	void create(const sf::View& view)
+	{
+		consumed = false;
+		const auto window = Application::get()->getWindow();
+		const auto mousePos = sf::Mouse::getPosition(*window);
+		m_Position = window->mapPixelToCoords(mousePos, view);
+		m_Pressed = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+	}
 
 	inline void consume() { consumed = true; }
 
 	// if(CursorRay) -> condition is true if the ray has not been consumed yet.
 	operator bool() const { return !consumed; }
 
-	const sf::Vector2i& getPosition() const { return m_Position; }
+	const sf::Vector2f& getPosition() const { return m_Position; }
 	const bool& isPressed() const { return m_Pressed; }
 private:
 	// When a CursorRay is used (example : it hovers a button, so the button changes his color and consumes the ray), the widget that uses it has to make this value as true. No verification are done when trying to access data. A widget can see data even if his consumed member is true. He just has to know that another widget has responded to the position member value.
 	bool consumed = false;
-	sf::Vector2i m_Position = sf::Mouse::getPosition(*Application::get()->getWindow());
-	bool m_Pressed = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+	sf::Vector2f m_Position;
+	bool m_Pressed = false;
 };
 
 class Widget
 {
 public:
 	Widget();
-	void update(const float& dt); // This function first calculates a CursorRay then internally calls an update function which takes this CursorRay as parameter.
-	virtual void render(Ref<sf::RenderWindow> window) {}
 
-	inline void setParent(Ref<Widget> parent) { m_Parent = parent; }
-	inline void addChild(Ref<Widget> child) { m_Children.push_back(child); }
+	// This function first calculates a CursorRay then calls update(const float&, CursorRay&).
+	void update(const float& dt);
+	// This function updates children then updates "this". More explainations in implementation.
+	void update(const float& dt, CursorRay& ray);
+	// This function first renders "this" then renders children. More explainations in implementation.
+	void render(Ref<sf::RenderWindow> window);
+	void onResize();
+	void onResize(const sf::Vector2f& size);
 
-	const sf::Vector2f& getGlobalPosition() const;
+	void setParent(Ref<Widget> parent);
+	void addChild(Ref<Widget> child);
+
+	sf::Vector2f getGlobalPosition() const;
 	inline const sf::Vector2f& getRelativePosition() const { return m_RelativePosition; }
 
-protected:
-	virtual void update(const float& dt, CursorRay& ray) {}
+	// In principle, this is the only place where we will work with "true" position values (here we convert percentage positions into "true" positions for various widget drawable components). This work has to be done in each Widget-derived class which holds drawable component(s).
+	virtual void setRelativePosition(const sf::Vector2f& pos);
+	// We usually shouldn't have to override this function because it is defined in therms of setRelativePosition().
+	virtual void setGlobalPosition(const sf::Vector2f& pos);
 
-	// We must first update children, then update this. If children have something to do with a ray, they handle it and then parent don't. If they don't handle the ray, their parent can handle it, recursively.
-	// Example : a Button widget has a Text widget as child. The Text widget first receives the CursorRay. It obviously has nothing to do with a ray, so it does not use it, and its "consumed" member stays false. Then the Button widget handles the ray. If the ray is inside its bounding box, it consumes it, else, it does not consume it (eventually the ray can be used by the Button widget's parent).
-	void updateChildren(const float& dt, CursorRay& ray); // For more readable code
-	// We must first render this, then children. Children have to show in front of their parent.
-	void renderChildren(Ref<sf::RenderWindow> window); // For more readable code
+	// Sets viewport to this widget and all children. Viewport is a sf::FloatRect where values are between 0 and 1 : the edges of the window.
+	void setViewport(const sf::FloatRect& viewport);
+
+protected: // Protected methods
+	/* Internal use only.
+	 * These methods shall update/render/trigger onResize the current Widget only. Current/children Widget(s) updating/rendering/onResize triggering order is managed by update(const float&, CursorRay&), render(Ref<sf::RenderWindow>) and onResize(const sf::Vector2f&). */
+	virtual void updateWidget(const float& dt, CursorRay& ray) {}
+	virtual void renderWidget(Ref<sf::RenderWindow> window) {}
+	virtual void onWidgetResize(const sf::Vector2f& size) {}
+
+private: // Private methods
+	// For more readable code in Widget class only, because they shouldn't be needed in Widget-derived classes.
+	void updateChildren(const float& dt, CursorRay& ray);
+	void renderChildren(Ref<sf::RenderWindow> window);
+	void onResizeChildren(const sf::Vector2f& size);
+	sf::IntRect getViewport() const;
 
 private:
 	Ref<Widget> m_Parent = nullptr;
 	std::vector<Ref<Widget>> m_Children;
 
+	Ref<sf::View> m_View;
+
+	// Values bewteen 0 and 1 : the edges of the Widget's viewport. "True" position value is processed each time we move a Widget and given to SFML drawables. This gives an abstraction to the programmer to use Widgets with percentages. We don't want to bother with variable position values (because of resizing and flexible viewport size).
 	sf::Vector2f m_RelativePosition;
 };
