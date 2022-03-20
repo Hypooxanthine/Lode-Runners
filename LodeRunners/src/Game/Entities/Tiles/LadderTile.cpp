f54e74a3c3d3c4a79146e491a46d49e5f866cdd9
Hypooxanthine <beaujonalexandre@gmail.com>
2022-03-20 20:46:16 +0100

    Extended ladder collider upwards.

--- LodeRunners/src/Game/Entities/Tiles/LadderTile.cpp
+++ LodeRunners/src/Game/Entities/Tiles/LadderTile.cpp
@@ -15,7 +15,8 @@ LadderTile::LadderTile(const bool& isExit)
 	m_SpriteComponent = makeComponent<SpriteComponent>("Sprite", Assets::getTile(TileType::Ladder).get());
 
 	m_Collider = makeComponent<ColliderComponent>("Collider");
-	m_Collider->setHitbox(m_SpriteComponent->getWorldSize());
+	m_Collider->setHitbox({1.f, 1.1f});
+	m_Collider->setRelativePosition({ 0.f, -.1f });
 	m_Collider->setCollisionType(CollisionType::Static);
 	m_Collider->setCollisionProfile(CollisionProfile::TileTransparent);
 }
